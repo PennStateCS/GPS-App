2993b0aa02cb2191cad6b0df736b68a3eb635663
Jeff Chiampi <jdc308@psu.edu>
2026-06-27 15:33:07 -0400

    Make Gradle wrapper executable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -198,6 +198,7 @@ dependencies {
 
     // --- Testing ---
     testImplementation(libs.junit)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("org.robolectric:robolectric:4.12.1")
     testImplementation("androidx.test:core:1.5.0")
     testImplementation("androidx.test.ext:junit:1.1.5")
